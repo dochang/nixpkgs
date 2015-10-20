f78e76cc3985ddbb2b412f36508046b7d879273f
Desmond O. Chang <dochang@gmail.com>
2015-10-20 11:02:02 +0800

    custom-packages.nix

---
+++ custom-packages.nix
@@ -0,0 +1,48 @@
+/*
+
+custom-nixpkgs
+==============
+
+Nix packages added or upgraded by myself.
+
+See:
+
+  - http://sandervanderburg.blogspot.com/2014/07/managing-private-nix-packages-outside.html
+  - https://nixos.org/wiki/Create_and_debug_nix_packages
+  - https://nixos.org/wiki/Debugging_a_Nix_Package
+  - https://nixos.org/wiki/Nix_Modifying_Packages
+  - http://fluffynukeit.com/adding-nix-expressions-for-new-packages/
+  - http://lethalman.blogspot.sg/2014/09/nix-pill-13-callpackage-design-pattern.html
+
+Usage
+-----
+
+### Build ###
+
+```
+nix-build custom-packages.nix -A <pkg>
+```
+
+### Install ###
+
+```
+nix-env --file custom-packages.nix --install <pkg>
+```
+
+*/
+
+{ system ? builtins.currentSystem }:
+
+let
+
+  pkgs = import <nixpkgs> { inherit system; };
+
+  callPackage = pkgs.lib.callPackageWith (pkgs // self);
+
+  self = {
+
+  };
+
+in
+
+self
